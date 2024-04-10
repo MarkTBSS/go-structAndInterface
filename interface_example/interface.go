e0cffe527fec3ff9f3c704d42aecfd6c2db0aa30
MARK\pirat <marktbss@gmail.com>
2024-04-10 10:13:55 +0700

    [MOD] Refactoring

--- interface_example/interface.go
+++ interface_example/interface.go
@@ -1,41 +1,37 @@
-package interfaceEx
+package interface_example
 
 import (
 	"fmt"
 )
 
-// ใช้ interface
-type Shape interface {
+type Shaper interface {
 	Area() float64
 	Perimeter() float64
 }
 
-// ใช้ struct Rectangle และ implement interface Shape
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
 // Constructor
-func NewRectangle(width, height float64) Shape {
+func NewRectangle(width, height float64) Shaper {
 	return &Rectangle{
 		Width:  width,
 		Height: height,
 	}
 }
 
-// Method สำหรับคำนวณพื้นที่ของ Rectangle
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-// Method สำหรับคำนวณเส้นรอบรูปของ Rectangle
 func (r Rectangle) Perimeter() float64 {
 	return 2*r.Width + 2*r.Height
 }
 
 // ใช้ฟังก์ชัน printShapeDetails เพื่อพิมพ์รายละเอียดของรูปทรงที่ได้รับ interface Shape
-func PrintShapeDetails(s Shape) {
+func PrintShapeDetails(s Shaper) {
 	area := s.Area()
 	perimeter := s.Perimeter()
 
--- main.go
+++ main.go
@@ -3,33 +3,17 @@ package main
 import (
 	"fmt"
 
-	interfaceEx "github.com/MarkTBSS/go-structAndInterface/interface"
+	"github.com/MarkTBSS/go-structAndInterface/interface_example"
+	"github.com/MarkTBSS/go-structAndInterface/no_interface"
 )
 
-// ไม่ใช้ interface
-type Rectangle struct {
-	Width  float64
-	Height float64
-}
-
-// ไม่ใช้ interface
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
-}
-
-// ไม่ใช้ interface
-func (r Rectangle) Perimeter() float64 {
-	return 2*r.Width + 2*r.Height
-}
-
 func main() {
-	rect := Rectangle{Width: 10, Height: 5}
+	rect := no_interface.Rectangle{Width: 10, Height: 5}
 	area := rect.Area()
 	perimeter := rect.Perimeter()
-
 	fmt.Printf("Area of the rectangle: %.2f square units\n", area)
 	fmt.Printf("Perimeter of the rectangle: %.2f units\n", perimeter)
 
-	rectInterface := interfaceEx.NewRectangle(100.00, 50.00)
-	interfaceEx.PrintShapeDetails(rectInterface)
+	rectInterface := interface_example.NewRectangle(100.00, 50.00)
+	interface_example.PrintShapeDetails(rectInterface)
 }
